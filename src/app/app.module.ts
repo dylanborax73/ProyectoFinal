24ca50ba99220608fd517c752a32564bf6084527
dylanborax73 <110707964+dylanborax73@users.noreply.github.com>
2023-05-12 16:47:11 -0300

    v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    RedesComponent,
+    BannerComponent,
+    SobremiComponent
   ],
   imports: [
     BrowserModule,
